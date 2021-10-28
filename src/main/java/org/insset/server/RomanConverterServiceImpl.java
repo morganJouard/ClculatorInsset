896756a88a3eb1182cbcc8c6cf6daabbfcb110ec
Florian Vaneste <florian@widop.com>
2021-10-28 23:18:54 +0200

    [FEAT][PSI-02] Convert roman to arabic

--- src/main/java/org/insset/server/RomanConverterServiceImpl.java
+++ src/main/java/org/insset/server/RomanConverterServiceImpl.java
@@ -33,6 +33,16 @@ public class RomanConverterServiceImpl extends RemoteServiceServlet implements R
         put(1, "I");
     }};
 
+    private final static TreeMap<Character, Integer> mapRomanToInt = new TreeMap<Character, Integer>() {{
+        put('I', 1);
+        put('V', 5);
+        put('X', 10);
+        put('L', 50);
+        put('C', 100);
+        put('D', 500);
+        put('M', 1000);
+    }};
+
     @Override
     public String convertDateYears(String nbr) throws IllegalArgumentException, NumberFormatException {
         int[] date;
@@ -48,8 +58,32 @@ public class RomanConverterServiceImpl extends RemoteServiceServlet implements R
 
     @Override
     public Integer convertRomanToArabe(String nbr) throws IllegalArgumentException {
-        //Implement your code
-        return 3;
+        int sum = 0;
+        int n = nbr.length();
+
+        if (nbr.contains("MM") && nbr.length() > 2) {
+            throw new IllegalArgumentException();
+        }
+
+        for(int i = 0; i < n; i++)
+        {
+
+            // If present value is less than next value,
+            // subtract present from next value and add the
+            // resultant to the sum variable.
+            if (i != n - 1
+                    && mapRomanToInt.get(nbr.charAt(i)) < mapRomanToInt.get(nbr.charAt(i + 1))
+            ) {
+                sum += mapRomanToInt.get(nbr.charAt(i + 1)) -
+                        mapRomanToInt.get(nbr.charAt(i));
+                i++;
+            }
+            else
+            {
+                sum += mapRomanToInt.get(nbr.charAt(i));
+            }
+        }
+        return sum;
     }
 
     @Override
